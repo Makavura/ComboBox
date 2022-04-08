08367e3f3c49666fc9fdd0d4bc0f91d51f9bb885
Makavura <makavuramughanga@gmail.com>
2022-04-08 11:24:17 +0300

    chore: declare variables and possible methods required for desired interactivity

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import countries from "./countries";
+
+const comboBoxInput = document.getElementById('combo-input-box');
+const comboBoxUpIcon = document.getElementById('combo-up-icon');
+const comboBoxDownIcon = document.getElementById('combo-down-icon');
+const comboBoxCountryListing = document.getElementById('combo-country-listing');
+const comboBoxLabel = document.getElementById('combo-input-label');
+
+(() =>{
+
+
+
+});
+
+const generateListing = (countries: Country[]) => {
+    countries.forEach((country: Country, index: number) => {
+
+    });
+};
+
+interface Country {
+    code: string;
+    label: string;
+    phone: string;
+};
+
+const showComboBoxListing = () => {
+
+};
+
+const hideComboBoxList = () => {
+
+};
+
+const showUpIcon = () => {
+
+};
+
+const showDownIcon = () => {
+    
+};
+
+const hideUpIcon = () => {
+
+};
+
+const hideDownIcon = () => {
+
+};
+
+const minimizeAndTranslateLabel = () => {
+
+};
+
+const setDefaultLabelState = () => {
+
+};
+
