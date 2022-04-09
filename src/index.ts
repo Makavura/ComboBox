7e791a7d6abb1c56771b2bb8813a10b9a4eb0f53
Makavura <makavuramughanga@gmail.com>
2022-04-09 14:00:48 +0300

    feature/update: generate country listing with flags

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 
 import countries from "./countries";
+import { countryFlags } from "./country.flags";
 // @ts-ignore
 import * as themeTogglerEventListener from './theme.toggler';
 
@@ -8,21 +9,62 @@ const comboBoxUpIcon = document.getElementById('combo-up-icon');
 const comboBoxDownIcon = document.getElementById('combo-down-icon');
 const comboBoxCountryListing = document.getElementById('combo-country-listing');
 const comboBoxLabel = document.getElementById('combo-input-label');
+const comboBoxCountryListingList = document.getElementById('combo-country-listing-list');
 
 const init = () => {
     generateListing(countries);
 };
 
 const generateListing = (countries: Country[]) => {
-    countries.forEach((country: Country, index: number) => {
+    let listing = ``;
+    let countriesWithFlags: CountryWithFlag[] = [];
 
-    }); 
+    countries.forEach((country: Country, index: number) => {
+        const flag: CountryFlag[] = countryFlags.filter((flag: CountryFlag) => {
+            return flag.code == country.code;
+        });
+        const flagEmoji: string = flag[0]?.emoji;
+        countriesWithFlags.push({ ...country, flag: flagEmoji });
+    });
+
+    countriesWithFlags.forEach((country: CountryWithFlag) => {
+        const listItem = `
+        <li class="flex flex-row w-full justify-between border border-gray-300 p-2">
+            <span class="grow-0">
+                ${country.flag}
+            </span>
+            <span>
+                ${country.code}
+            </span>
+            <span>
+                ${country.phone}
+            </span>
+            <p>
+                ${country.label}
+            </p>
+        </li>
+        `;
+        listing += listItem;
+    });
+    comboBoxCountryListingList?.append(listing);
 };
 
 interface Country {
     code: string;
     label: string;
     phone: string;
+};
+
+export interface CountryFlag {
+    emoji: string;
+    unicode: string;
+    image: string;
+    name: string;
+    code: string;
+};
+
+export interface CountryWithFlag extends Country {
+    flag: string;
 };
 
 const showComboBoxListing = () => {
@@ -77,5 +119,7 @@ const updateInputWithSelectedListItem = (selection: string) => {
     // @ts-ignore
     comboBoxInput?.nodeValue = selection;
 };
+
+
 
 init();
